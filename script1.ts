5fcbf052df6af18c04243c5900d2db2834287dfd
unknown <lautaro.kaza@gmail.com>
2024-10-26 22:30:18 -0300

    Remove redundant entries

--- script1.ts
+++
@@ -1,8 +0,0 @@
-import { ChessBoard } from 'app\chess-logic\chess-board'
-import { Color, FENChar } from './app/chess-logic/models';
-
-export class ChessBoardComponent {
-    private ChessBoard = new ChessBoard();
-    public chessBoardView: (FENChar | null)[][] = this.chessBoardView.chessBoardView;
-    public get playerColor(): Color{return this.chessBoard.playerColor;};
-}
